028aa04bf36c55319720e8cbdf83f43b51406ad5
hksuri <huzaifasuri@gmail.com>
2023-03-09 18:54:06 -0600

    updated docker files

--- launch.sh
+++ launch.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run --rm --gpus=all --shm-size 90G --ipc=host -v /home/huzaifa:/home/huzaifa -v /mnt/ssd_2tb_1:/mnt/ssd_2tb_1 -v /mnt/ssd_4tb_0:/mnt/ssd_4tb_0 -v /mnt/hdd_16tb_0:/mnt/hdd_16tb_0 -v /scr:/scr -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -w /home/huzaifa --user="$(id -u):$(id -g)" --volume="$PWD:/app" -p 2198:2198 -it huzaifasuri/vivaldi:mayo_retina
+docker run --rm --gpus=all --shm-size 90G --ipc=host -v /home/huzaifa:/home/huzaifa -v /mnt/ssd_2tb_1:/mnt/ssd_2tb_1 -v /mnt/ssd_4tb_0:/mnt/ssd_4tb_0 -v /mnt/hdd_16tb_0:/mnt/hdd_16tb_0 -v /scr:/scr -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -w /home/huzaifa --user="$(id -u):$(id -g)" --volume="$PWD:/app" -p 8892:8892 -it huzaifasuri/vivaldi:mayo_retina
+
+# docker run --rm --gpus=all --shm-size 90G --ipc=host -v /home/huzaifa:/home/huzaifa -v /mnt/ssd_2tb_1:/mnt/ssd_2tb_1 -v /mnt/ssd_4tb_0:/mnt/ssd_4tb_0 -v /mnt/hdd_16tb_0:/mnt/hdd_16tb_0 -v /scr:/scr -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -w /home/huzaifa --user="$(id -u):$(id -g)" --volume="$PWD:/app" -it huzaifasuri/vivaldi:mayo_retina
