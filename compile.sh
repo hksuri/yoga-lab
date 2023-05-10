f9076ae0756c4bcc2fe5784d1b24bc470d082d30
hksuri <huzaifasuri@gmail.com>
2023-05-10 17:29:56 -0500

    latest commit

--- compile.sh
+++ compile.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker build -t huzaifasuri/vivaldi:mayo_retina -f Dockerfile.hks .
-docker push huzaifasuri/vivaldi:mayo_retina
+# docker build -t huzaifasuri/vivaldi:mayo_retina -f Dockerfile.hks .
+# docker push huzaifasuri/vivaldi:mayo_retina
+
+docker build -t huzaifasuri/vivaldi:fpn_5 -f Dockerfile.hks .
+docker push huzaifasuri/vivaldi:fpn_5
