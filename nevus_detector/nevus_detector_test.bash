9868828ccc8c1438363736e733904ba526209640
hksuri <huzaifasuri@gmail.com>
2023-11-01 18:34:20 -0500

    remove cd command

--- nevus_detector/nevus_detector_test.bash
+++ nevus_detector/nevus_detector_test.bash
@@ -4,14 +4,11 @@
 #SBATCH --gres=gpu:1     # Request 1 GPU
 #SBATCH --time=24:00:00  # Maximum runtime (adjust as needed)
 #SBATCH --mem=8G         # Request 8GB of memory (adjust as needed)
-#SBATCH --output=./slurm_jobs/output%x.%j.stdout
-#SBATCH --error=./slurm_jobs/error%x.%j.stderr
+#SBATCH --output=/research/labs/ophthalmology/iezzi/m294666/nevus_detector_slurm_jobs/output%x.%j.stdout
+#SBATCH --error=/research/labs/ophthalmology/iezzi/m294666/nevus_detector_slurm_jobs/error%x.%j.stderr
 #SBATCH --mail-user=suri.muhammadhuzaif@mayo.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --chdir /research/labs/ophthalmology/iezzi/m294666/yoga-lab/nevus_detector
-
-# Navigate to the directory where your Python script is located
-# cd /research/labs/ophthalmology/iezzi/m294666/yoga-lab/nevus_detector
 
 # Run your Python script, and capture both stdout and stderr
 python train_val.py
