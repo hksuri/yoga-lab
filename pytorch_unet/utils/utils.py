ce4cf037f59c7deb1caacbfe9122bc63c7e3ec5c
hksuri <huzaifasuri@gmail.com>
2024-04-04 13:01:05 -0500

    Modify plot function to plot original, masked, output

--- pytorch_unet/utils/utils.py
+++ pytorch_unet/utils/utils.py
@@ -29,21 +29,29 @@ def plot_train_val_loss(train_loss, val_loss, save_dir):
     plt.savefig(f"{save_dir}/loss.png")
     plt.close()
 
-def plot_img_and_mask(img_in, img_out, mask, img_name, dir_output, mode = 'inpainted image'):
+def plot_img_and_mask(img_org, img_in, img_out, mask, img_name, dir_output, mode = 'inpainted image'):
 
     file_path = dir_output + str(img_name).strip("[]'").split('.')[0] + ".png"
     
+    # Normalizing and converting original image
+    img = img_org
+    img = img.squeeze(0).permute(1, 2, 0).cpu().numpy()
+    img = np.clip(img * 255, 0, 255).astype(np.uint8)
+    
     # Normalizing and converting input image
-    image_normalized_in = (img_in + 1) / 2
+    # image_normalized_in = (img_in + 1) / 2
+    image_normalized_in = img_in
     image_normalized_in = image_normalized_in.squeeze(0).permute(1, 2, 0).cpu().numpy()
     image_back_in = np.clip(image_normalized_in * 255, 0, 255).astype(np.uint8)
 
     # Normalizing and converting outut image
-    image_normalized_out = (img_out + 1) / 2
+    # image_normalized_out = (img_out + 1) / 2
+    image_normalized_out = img_out
     image_normalized_out = image_normalized_out.squeeze(0).permute(1, 2, 0).cpu().numpy()
     image_back_out = np.clip(image_normalized_out * 255, 0, 255).astype(np.uint8)
 
     # Convert numpy arrays to PIL images
+    image_back_org_pil = Image.fromarray(img)
     image_back_in_pil = Image.fromarray(image_back_in)
     image_back_out_pil = Image.fromarray(image_back_out)
 
@@ -52,15 +60,18 @@ def plot_img_and_mask(img_in, img_out, mask, img_name, dir_output, mode = 'inpai
         image_back_out_pil = Image.fromarray(image_back_out * mask.transpose(1, 2, 0))
 
     # Creating a new image with the appropriate dimensions
-    width = image_back_in_pil.width + image_back_out_pil.width
-    height = max(image_back_in_pil.height, image_back_out_pil.height)
+    width = image_back_org_pil.width + image_back_in_pil.width + image_back_out_pil.width
+    height = max(image_back_org_pil.height, image_back_in_pil.height, image_back_out_pil.height)
     combined_img = Image.new('RGB', (width, height))
 
-    # Place image_back_in on the left
-    combined_img.paste(image_back_in_pil, (0, 0))
+    # Place image_back_org on the left
+    combined_img.paste(image_back_org_pil, (0, 0))
 
-    # Place image_back_out or its processed form on the right
-    combined_img.paste(image_back_out_pil, (image_back_in_pil.width, 0))
+    # Place image_back_in in the middle
+    combined_img.paste(image_back_in_pil, (image_back_org_pil.width, 0))
+
+    # Place image_back_out on the right
+    combined_img.paste(image_back_out_pil, (image_back_org_pil.width + image_back_in_pil.width, 0))
 
     # Save the combined image
     combined_img.save(file_path)
