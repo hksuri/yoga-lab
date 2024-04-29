17ee0dd9d554b9583dc214a75173ae9be699c898
hksuri <huzaifasuri@gmail.com>
2024-04-28 23:19:40 -0500

    Add resnet commands

--- RETFound_MAE/retfound.bash
+++ RETFound_MAE/retfound.bash
@@ -2,7 +2,7 @@
 #SBATCH --job-name=retfound_dia5_huzaifa
 #SBATCH --partition=gpu  # Specify the GPU partition
 #SBATCH --gres=gpu:1     # Request 1 GPU
-#SBATCH --time=24:00:00  # Maximum runtime (adjust as needed)
+#SBATCH --time=48:00:00  # Maximum runtime (adjust as needed)
 #SBATCH --mem=64G         # Request memory (adjust as needed)
 #SBATCH --output=/research/labs/ophthalmology/iezzi/m294666/retfound_slurm_jobs/output_%x.%j.stdout
 #SBATCH --error=/research/labs/ophthalmology/iezzi/m294666/retfound_slurm_jobs/error_%x.%j.stderr
@@ -16,7 +16,7 @@
 # python main_finetune.py --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_orange/' --rf 'orange' --log_task '/orange/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name'
 
 # Train + Eval (CF only)
-python main_finetune.py --epochs 100 --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_dia5_CF/' --rf 'dia5' --log_task '/dia5/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
+# python main_finetune.py --epochs 200 --batch_size 32 --save_images --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_dia5_CF/' --rf 'dia5' --log_task '/dia5/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
 # python main_finetune.py --epochs 100 --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_intref_CF/' --rf 'intref' --log_task '/intref/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
 # python main_finetune.py --epochs 100 --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_orange_CF/' --rf 'orange' --log_task '/orange/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
 
@@ -30,6 +30,13 @@ python main_finetune.py --epochs 100 --task '/research/labs/ophthalmology/iezzi/
 # python main_finetune.py --eval --save_images --resume '/research/labs/ophthalmology/iezzi/m294666/retfound_task_intref_CF/checkpoint-best-frozen-encoder.pth' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_intref_CF/' --rf 'intref' --log_task '/intref/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
 # python main_finetune.py --eval --save_images --resume '/research/labs/ophthalmology/iezzi/m294666/retfound_task_orange_CF/checkpoint-best-frozen-encoder.pth' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_orange_CF/' --rf 'orange' --log_task '/orange/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
 
+# UNet Training
+# python train.py --epochs 100
+
+# ResNet Training
+python main_finetune.py --epochs 100 --batch_size 32 --input_size 1024 --save_images --resnet_model_name 'resnet18' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_dia5_CF/' --rf 'dia5' --log_task '/dia5/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
+# python main_finetune.py --epochs 100 --batch_size 32 --input_size 1024 --save_images --resnet_model_name 'resnet18' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_intref_CF/' --rf 'intref' --log_task '/intref/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
+# python main_finetune.py --epochs 100 --batch_size 32 --input_size 1024 --save_images --resnet_model_name 'resnet18' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_orange_CF/' --rf 'orange' --log_task '/orange/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
 
 # Job complete
 echo "Job completed on $(date)"
