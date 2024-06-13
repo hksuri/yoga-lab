3cd64c534ceb2840aa3180b36e76b71401d45df0
hksuri <huzaifasuri@gmail.com>
2024-06-13 12:17:48 -0500

    Add line to run dinov2.py + new path for data

--- RETFound_MAE/retfound.bash
+++ RETFound_MAE/retfound.bash
@@ -16,12 +16,15 @@
 # python main_finetune.py --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_orange/' --rf 'orange' --log_task '/orange/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name'
 
 # Train + Eval (CF only)
-# python main_finetune.py --epochs 200 --batch_size 32 --save_images --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_dia5_CF/' --rf 'dia5' --log_task '/dia5/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
-# python main_finetune.py --epochs 100 --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_intref_CF/' --rf 'intref' --log_task '/intref/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
-# python main_finetune.py --epochs 100 --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_orange_CF/' --rf 'orange' --log_task '/orange/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
+python main_finetune.py --discard_ratio 0.5 --head_fusion 'mean' --epochs 200 --save_images --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_dia5_CF/' --rf 'dia5' --log_task '/dia5/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_June2024_final'
+# python main_finetune.py --epochs 100 --freeze --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_intref_CF/' --rf 'intref' --log_task '/intref/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_June2024_final'
+# python main_finetune.py --epochs 100 --freeze --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_orange_CF/' --rf 'orange' --log_task '/orange/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_June2024_final'
+# python main_finetune.py --epochs 100 --freeze --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_va_CF/' --rf 'va' --log_task '/va/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_June2024_final'
+# python main_finetune.py --epochs 100 --freeze --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_thick2_CF/' --rf 'thick2' --log_task '/thick2/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_June2024_final'
+# python main_finetune.py --epochs 100 --freeze --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_srf_CF/' --rf 'srf' --log_task '/srf/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_June2024_final'
 
 # Eval only
-# python main_finetune.py --eval --resume '/research/labs/ophthalmology/iezzi/m294666/retfound_task_dia5/checkpoint-best.pth' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_dia5/' --rf 'dia5' --log_task '/dia5/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name'
+# python main_finetune.py --eval --save_images --resume '/research/labs/ophthalmology/iezzi/m294666/retfound_task_dia5/checkpoint-best.pth' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_dia5_CF/' --rf 'dia5' --log_task '/dia5/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_June2024_final'
 # python main_finetune.py --eval --resume '/research/labs/ophthalmology/iezzi/m294666/retfound_task_intref/checkpoint-best.pth' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_intref/' --rf 'intref' --log_task '/intref/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name'
 # python main_finetune.py --eval --resume '/research/labs/ophthalmology/iezzi/m294666/retfound_task_orange/checkpoint-best.pth' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_orange/' --rf 'orange' --log_task '/orange/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name'
 
@@ -34,9 +37,12 @@
 # python train.py --epochs 100
 
 # ResNet Training
-python main_finetune.py --epochs 100 --batch_size 32 --input_size 1024 --save_images --resnet_model_name 'resnet18' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_dia5_CF/' --rf 'dia5' --log_task '/dia5/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
-# python main_finetune.py --epochs 100 --batch_size 32 --input_size 1024 --save_images --resnet_model_name 'resnet18' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_intref_CF/' --rf 'intref' --log_task '/intref/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
-# python main_finetune.py --epochs 100 --batch_size 32 --input_size 1024 --save_images --resnet_model_name 'resnet18' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_orange_CF/' --rf 'orange' --log_task '/orange/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
+# python main_finetune.py --epochs 300 --batch_size 32 --input_size 1024 --save_images --resnet_model_name 'resnet18' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_dia5_CF/' --rf 'dia5' --log_task '/dia5/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
+# python main_finetune.py --epochs 300 --batch_size 32 --input_size 1024 --save_images --resnet_model_name 'resnet18' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_intref_CF/' --rf 'intref' --log_task '/intref/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
+# python main_finetune.py --epochs 300 --batch_size 32 --input_size 1024 --save_images --resnet_model_name 'resnet18' --task '/research/labs/ophthalmology/iezzi/m294666/retfound_task_orange_CF/' --rf 'orange' --log_task '/orange/' --data_path '/research/labs/ophthalmology/iezzi/m294666/nevus_data_500_risk_factors_processed_RF_in_name_CF_only'
+
+# Dinov2 Embedding Obtaining
+python dino.py
 
 # Job complete
 echo "Job completed on $(date)"
